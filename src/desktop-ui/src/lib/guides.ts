86a247fdf9adc1911a9b22bc0cb14c9cf4f0ebaf
sweeping-monk886 <2424814623@qq.com>
2026-08-18 16:06:47 +0800

    fix(desktop): update Codex guide link

--- src/desktop-ui/src/lib/guides.ts
+++ src/desktop-ui/src/lib/guides.ts
@@ -1,4 +1,4 @@
 export const AI_MODEL_GUIDE_URL =
-  "https://xcnikjydrc0g.feishu.cn/wiki/W774wTGbCiFpUUkDCUXcAUQznqh?from=from_copylink";
+  "https://pcnj98w1q30x.feishu.cn/wiki/ASIGwYdMPi6SQLkaIdNc9r6Ln7c?from=from_copylink";
 
 export const GATEWAY_TOKEN_URL = "https://ai.939593.xyz/console/token";
